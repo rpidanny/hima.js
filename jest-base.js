7eff32ebdfe808a82aec3990caf1ec25f29e0f9e
rpidanny <abhishekmaharjan1993@gmail.com>
2020-08-04 21:24:10 +0200

    fix: add missing ts/jest config files

---
+++ jest-base.js
@@ -0,0 +1,12 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+}
