5aab87524c8b553e7cc7257b7667d83c86b35976
rpidanny <abhishekmaharjan1993@gmail.com>
2020-08-07 07:34:24 +0200

    feat: add downloadImages cli command

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,14 @@
 import path from 'path'
-import { Command } from 'commander'
+import { program } from 'commander'
 import cliProgress from 'cli-progress'
 import colors from 'colors' // TODO: replace with chalk
 
-import { downloadImage } from './index'
+import { downloadImage, downloadImages } from './index'
 import { ProgressFunction } from './types'
 
-interface InputOpts {
+import packageJson from '../package.json'
+
+interface ImageInputOpts {
   out: string
   date: string
   zoom: string
@@ -16,8 +18,20 @@ interface InputOpts {
   quiet: boolean
 }
 
-const getProgressHandler = (inputOpts: InputOpts): ProgressFunction | undefined => {
-  if (inputOpts.quiet) {
+interface ImagesInputOpts {
+  out: string
+  startDate: string
+  endDate: string
+  interval: number
+  zoom: string
+  ir: boolean
+  batchSize: string
+  debug: boolean
+  quiet: boolean
+}
+
+const getProgressHandler = (quiet: boolean): ProgressFunction | undefined => {
+  if (quiet) {
     return
   }
   const progressBar = new cliProgress.SingleBar(
@@ -52,23 +66,7 @@ const getProgressHandler = (inputOpts: InputOpts): ProgressFunction | undefined 
   }
 }
 
-async function main(): Promise<void> {
-  const rootPath = process.cwd()
-
-  const program = new Command()
-
-  program.option('--out <path>', 'output directory', rootPath)
-  program.option('--date <date>', 'Date in yyyy/mm/dd hh:mm:ss', 'latest')
-  program.option('--zoom <level>', 'Zoom level. 1-3 for IR. 1-5 for color', '1')
-  program.option('--ir', 'Download Infrared Image', false)
-  program.option('--batch-size', 'How many tiles to download in parallel?', '20')
-  program.option('--debug', 'Enable debug logs?', false)
-  program.option('--quiet', 'Disable all logs?', false)
-
-  program.parse(process.argv)
-
-  const inputOpts = program.opts() as InputOpts
-
+const handleImageCommand = async (inputOpts: ImageInputOpts): Promise<void> => {
   const { output } = await downloadImage({
     date: inputOpts.date,
     output: path.resolve(inputOpts.out),
@@ -76,12 +74,63 @@ async function main(): Promise<void> {
     batchSize: (inputOpts.batchSize && parseInt(inputOpts.batchSize)) || 20,
     infrared: inputOpts.ir,
     debug: inputOpts.quiet || inputOpts.debug,
-    progress: getProgressHandler(inputOpts),
+    progress: getProgressHandler(inputOpts.quiet),
   })
 
   if (!inputOpts.quiet && output) {
     console.log(colors.green.inverse(`File saved at ${output}`))
   }
+}
+
+const handleImagesCommand = async (inputOpts: ImagesInputOpts): Promise<void> => {
+  const { images } = await downloadImages({
+    startDate: inputOpts.startDate,
+    endDate: inputOpts.endDate,
+    interval: inputOpts.interval,
+    output: path.resolve(inputOpts.out),
+    zoom: parseInt(inputOpts.zoom),
+    batchSize: (inputOpts.batchSize && parseInt(inputOpts.batchSize)) || 20,
+    infrared: inputOpts.ir,
+    debug: inputOpts.quiet || inputOpts.debug,
+    progress: getProgressHandler(inputOpts.quiet),
+  })
+  if (!inputOpts.quiet && images) {
+    console.log(colors.green.inverse(`${images.length} Images saved to ${inputOpts.out}`))
+  }
+}
+
+async function main(): Promise<void> {
+  const rootPath = process.cwd()
+
+  program.version(packageJson.version)
+
+  program
+    .command('image [env]')
+    .description('Download single image')
+    .option('--out <path>', 'output directory', rootPath)
+    .option('--date <date>', 'Date in yyyy/mm/dd hh:mm:ss', 'latest')
+    .option('--zoom <level>', 'Zoom level. 1-3 for IR. 1-5 for color', '1')
+    .option('--ir', 'Download Infrared Image', false)
+    .option('--batch-size', 'How many tiles to download in parallel?', '20')
+    .option('--debug', 'Enable debug logs?', false)
+    .option('--quiet', 'Disable all logs?', false)
+    .action(async (_env, options) => await handleImageCommand(options))
+
+  program
+    .command('images [options]')
+    .description('Download multiple image')
+    .option('--out <path>', 'Output directory', rootPath)
+    .option('--start-date <date>', 'Date in yyyy/mm/dd hh:mm:ss')
+    .option('--end-date <date>', 'Date in yyyy/mm/dd hh:mm:ss')
+    .option('--interval <minutes>', 'Date in yyyy/mm/dd hh:mm:ss', '10')
+    .option('--zoom <level>', 'Zoom level. 1-3 for IR. 1-5 for color', '1')
+    .option('--ir', 'Download Infrared Image', false)
+    .option('--batch-size', 'How many tiles to download in parallel?', '20')
+    .option('--debug', 'Enable debug logs?', false)
+    .option('--quiet', 'Disable all logs?', false)
+    .action(async (_env, options) => await handleImagesCommand(options))
+
+  program.parse(process.argv)
 }
 
 try {
