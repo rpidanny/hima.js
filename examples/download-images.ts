6eb74c92927738168fb014b24cc42fc3b66ce4a6
rpidanny <abhishekmaharjan1993@gmail.com>
2020-08-07 06:30:29 +0200

    feat: add progress function for downloadImages

--- examples/download-images.ts
+++ examples/download-images.ts
@@ -1,12 +1,13 @@
 import { downloadImages } from '../src'
 
 downloadImages({
-  zoom: 1,
-  batchSize: 20,
+  zoom: 2,
+  batchSize: 5,
   startDate: '2020/02/14 05:00:00',
   endDate: '2020/02/14 10:00:00',
   interval: 30, // 30 minutes
-  debug: true,
+  // debug: true,
+  progress: (c, t) => console.log(`${(c / t) * 100}% complete`),
 })
   .then(console.log)
   .catch(console.error)
--- src/usecases/download-images/steps/download-dates.ts
+++ src/usecases/download-images/steps/download-dates.ts
@@ -11,6 +11,7 @@ export default async (ctx: Context, next: NextFunction): Promise<void> => {
 
     if (dates && output) {
       const responses: Array<imageTypes.Success> = []
+      let totalProgress = 0
       for (let i = 0; i < dates.length; i++) {
         const response: imageTypes.Success = await downloadImage({
           date: dates[i],
@@ -20,10 +21,18 @@ export default async (ctx: Context, next: NextFunction): Promise<void> => {
           debug,
           zoom,
           output: path.resolve(output, `${dates[i].toUTCString()}.jpg`),
+          progress: (c, t) => {
+            if (progress) {
+              const percentComplete = (c / t) * 100
+              if (percentComplete === 100) {
+                totalProgress += 100
+                progress(totalProgress, dates.length * 100)
+              } else {
+                progress(totalProgress + percentComplete, dates.length * 100)
+              }
+            }
+          },
         })
-        if (progress) {
-          progress(i + 1, dates.length)
-        }
         responses.push(response)
       }
 
