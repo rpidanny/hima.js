285737743ebcd259a358320d25d5dc8f657c6452
rpidanny <abhishekmaharjan1993@gmail.com>
2020-08-08 16:09:04 +0200

    fix: check file exists before downloading images

--- src/usecases/create-timelapse/steps/options-parser.ts
+++ src/usecases/create-timelapse/steps/options-parser.ts
@@ -1,4 +1,6 @@
+import fs from 'fs-extra'
 import mkTmp, { DirResult } from 'tmp'
+import { promisify } from 'util'
 
 import { downloadImages } from '../../download-images'
 import { Context } from '../types'
@@ -6,14 +8,21 @@ import { NextFunction } from '../../../types'
 import { getLogger } from '../../../utils/logger'
 import { getZoomFromQuality } from '../../../utils/video'
 
+const pathExists = promisify(fs.pathExists)
+
 export default async (ctx: Context, next: NextFunction): Promise<void> => {
   const { options } = ctx
 
   if (options) {
-    const { debug, quality } = options
+    const { debug, quality, output } = options
 
-    if (quality !== undefined && debug !== undefined) {
+    if (quality !== undefined && debug !== undefined && output) {
       const log = getLogger(debug)
+
+      if (await pathExists(output)) {
+        throw new Error(`${output} already exists.`)
+      }
+
       const tempDir: DirResult = mkTmp.dirSync({ unsafeCleanup: true })
 
       const commonOptions = { ...options }
