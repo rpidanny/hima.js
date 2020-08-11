c6b9413c246cd89e7af684c55a899ba7e1225bdc
rpidanny <abhishekmaharjan1993@gmail.com>
2020-08-11 12:18:18 +0200

    test: set test dates to 2020

--- test/integration/images.spec.ts
+++ test/integration/images.spec.ts
@@ -1,27 +1,27 @@
-// import mktemp, { DirResult } from 'tmp'
+import mktemp, { DirResult } from 'tmp'
 
 import { downloadImages } from '../../src'
-// import * as types from '../../src/usecases/download-images/types'
+import * as types from '../../src/usecases/download-images/types'
 
-const BATCH_SIZE = 50
+const BATCH_SIZE = 5
 
 describe('Hima images module', () => {
-  // describe('Download color images between two dates', () => {
-  //   it('should run without fail', async () => {
-  //     const tempDir: DirResult = mktemp.dirSync({ unsafeCleanup: true })
-  //     const response: types.Success = await downloadImages({
-  //       startDate: '2019/10/21 05:00:00',
-  //       endDate: '2019/10/21 06:00:00',
-  //       interval: 30, // 30 minutes
-  //       output: tempDir.name,
-  //       batchSize: BATCH_SIZE,
-  //       zoom: 1,
-  //     })
-  //     const resp = await tempDir.removeCallback()
-  //     expect(response.images.length).toBe(2)
-  //     return resp
-  //   })
-  // })
+  describe('Download color images between two dates', () => {
+    it('should run without fail', async () => {
+      const tempDir: DirResult = mktemp.dirSync({ unsafeCleanup: true })
+      const response: types.Success = await downloadImages({
+        startDate: '2020/02/14 05:00:00',
+        endDate: '2020/02/14 06:00:00',
+        interval: 30, // 30 minutes
+        output: tempDir.name,
+        batchSize: BATCH_SIZE,
+        zoom: 1,
+      })
+      const resp = await tempDir.removeCallback()
+      expect(response.images.length).toBe(2)
+      return resp
+    })
+  })
 
   describe('Validation Tests', () => {
     it('should fail when zoom value for infrared image is more than 3', async () => {
@@ -30,8 +30,8 @@ describe('Hima images module', () => {
           zoom: 4,
           batchSize: BATCH_SIZE,
           infrared: true,
-          startDate: '2019/10/21 05:00:00',
-          endDate: '2019/10/21 06:00:00',
+          startDate: '2020/02/14 05:00:00',
+          endDate: '2020/02/14 06:00:00',
           interval: 30, // 30 minutes
         })
       } catch (err) {
@@ -45,8 +45,8 @@ describe('Hima images module', () => {
           zoom: 6,
           batchSize: BATCH_SIZE,
           infrared: false,
-          startDate: '2019/10/21 05:00:00',
-          endDate: '2019/10/21 06:00:00',
+          startDate: '2020/02/14 05:00:00',
+          endDate: '2020/02/14 06:00:00',
           interval: 30, // 30 minutes
         })
       } catch (err) {
@@ -60,8 +60,8 @@ describe('Hima images module', () => {
           zoom: 1,
           batchSize: BATCH_SIZE,
           infrared: false,
-          startDate: '2019/10/21 05:00:00',
-          endDate: '2019/10/21 06:00:00',
+          startDate: '2020/02/14 05:00:00',
+          endDate: '2020/02/14 06:00:00',
           interval: 3, // 3 minutes
         })
       } catch (err) {
