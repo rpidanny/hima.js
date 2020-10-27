1b0231610f87ddc307a358a77a27f68666e979ba
Abhishek Maharjan <abhishekmaharjan1993@gmail.com>
2020-10-27 18:23:04 +0100

    test: nock get latest date request (#94)

--- test/unit/dates.spec.ts
+++ test/unit/dates.spec.ts
@@ -1,22 +1,55 @@
+import nock from 'nock'
+
 import { getLogger } from '../../src/utils/logger'
 import { parseDate, getDates } from '../../src/utils/dates'
 import { createImageOptions } from '../utils/create-data'
 import * as types from '../../src/types'
 import * as imageTypes from '../../src/usecases/download-image/types'
+import config from '../../src/config'
 
 describe('Utils: Check Date Helpers', () => {
   let log: types.LogFunction
+
   beforeAll(() => {
     log = getLogger(true, 'test')
   })
+
+  beforeEach(() => {
+    nock.abortPendingRequests()
+    nock.cleanAll()
+    nock.disableNetConnect()
+  })
+
+  afterEach(() => {
+    const pending = nock.pendingMocks()
+
+    if (pending.length > 0) {
+      console.log('Pending Nocks: ', pending)
+      throw new Error(`${pending.length} mocks are pending!`)
+    }
+
+    nock.enableNetConnect()
+  })
+
   it('should get latest date', async () => {
+    const currentDate: Date = new Date()
+
+    nock(config.baseUrl)
+      .get('/D531106/latest.json')
+      .reply(
+        200,
+        JSON.stringify({
+          date: currentDate.toISOString(),
+          file: 'PI_H08_20201027_1650_TRC_FLDK_R10_PGPFD.png',
+        }),
+      )
+
     const options: imageTypes.Options = createImageOptions({ date: 'latest', log })
     const latestDate: Date = await parseDate(options)
-    const currentDate: Date = new Date()
+
     expect(latestDate.getFullYear()).toBe(currentDate.getFullYear())
-    // Due to timezones, this fails once in a while
-    // expect(latestDate.getMonth()).toBe(currentDate.getMonth())
-    // expect(latestDate.getDay()).toBe(currentDate.getDay())
+    expect(latestDate.getMonth()).toBe(currentDate.getMonth())
+    expect(latestDate.getDay()).toBe(currentDate.getDay())
   })
   it('should parse date string correctly', async () => {
     const options: imageTypes.Options = createImageOptions({ date: '2019/10/21 18:30:21Z', log })
